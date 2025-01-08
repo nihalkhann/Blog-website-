7ae592a8a0e433acf7622649c06cff0a36fb78da
nihalkhann <144270394+nihalkhann@users.noreply.github.com>
2025-01-08 13:22:48 +0500

    Update client.ts

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,10 +1,12 @@
+
 import { createClient } from 'next-sanity'
 
 // import { apiVersion, dataset, projectId } from '../env'
 
 export const client = createClient({
-  projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'evuxjifk',
-  dataset : process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
+  projectId : process.env.NEXT_PUBLIC_SANITY_PROJECT_ID ,
+  dataset : process.env.NEXT_PUBLIC_SANITY_DATASET,
   apiVersion :'v2025-01-03',
   useCdn: true, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
+
